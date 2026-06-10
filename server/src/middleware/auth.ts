bc4c7f9eda237a4ecb33391ae406ea86295ebbd3
Trinh Vo <62305589+trinhvo20@users.noreply.github.com>
2026-06-10 09:54:18 -0500

    Set up Better Auth for authentication

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from 'express'
+import { fromNodeHeaders } from 'better-auth/node'
+import { auth } from '../lib/auth'
+
+type Session = typeof auth.$Infer.Session
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: Session['user']
+      session: Session['session']
+    }
+  }
+}
+
+export async function requireAuth(req: Request, res: Response, next: NextFunction) {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  })
+
+  if (!session) {
+    return res.status(401).json({ error: 'Unauthorized' })
+  }
+
+  req.user = session.user
+  req.session = session.session
+  next()
+}
